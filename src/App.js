ccf3c1bd8e83d9dd16d4d713657720e0b8f7a917
VLD202 <dixitvijaylaxmi202@gmail.com>
2025-11-04 16:35:53 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import TestAPI from './components/testapi';
+// import TestAPI from './components/testapi';
 import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+   <div>
+    <Dashboard />
+   </div>
   );
 }
 
